2bd389749d9d92e78de4e739dd2eccf95f55743c
AnnaEssa <109094406+AnnaEscrisa@users.noreply.github.com>
2024-03-17 19:01:28 +0100

    changed_about

--- js/about-data.js
+++ js/about-data.js
@@ -5,9 +5,9 @@ let langContent = {
         pageTitle: 'About Me',
         header: '⫶About Me⫶',
         subtitle1: 'Who am I?',
-        desc1: 'I am Anna Escribano, a junior full-stack developer based in Spain.',
-        desc2: 'I started my journey in web development in 2022 when I enrolled in the Open Source Back-End Web Development bootcamp. During this course, I discovered my passion for programming and became interested in the Full Stack programmer profile. To further develop my skills, I completed a Front-End Web Development bootcamp, which helped me to become proficient in both front-end and back-end development.',
-        desc3: 'In addition to my technical skills, I have a degree in East Asian Studies. I spent time studying in Japan, where I learned to collaborate with people from diverse backgrounds and adapt to new situations. My interest in art has also been a lifelong pursuit, and I continue to develop my skills as a hobbyist artist in my free time.',
+        desc1: 'I am Anna Escribano, a web development student based in Spain.',
+        desc2: 'I started my journey in web development in 2022 when I enrolled in a back-end development bootcamp. During this course, I discovered my passion for programming and decided to take a course in front-end development. Currently, I am undertaking Professional Training in Web Development.',
+        desc3: 'In addition to my technical skills, I have a degree in East Asian Studies and I am a hobbyist artist. I spent some time studying in Japan, where I learned to collaborate with people from diverse backgrounds and adapt to new situations.',
         desc4: "Whether I'm coding or drawing, I strive to bring creativity and attention to detail to everything I do.",
         subtitle2: 'Tech stack'
     },
@@ -15,9 +15,9 @@ let langContent = {
         pageTitle: 'Sobre Mi',
         header: '⫶Sobre Mi⫶',
         subtitle1: '¿Quién Soy?',
-        desc1: 'Soy Anna Escribano, desarrolladora full-stack junior.',
-        desc2: 'Empecé a formarme en esta disciplina en 2022, cuando me inscribí en el bootcamp Open Source Back-End Web Development. Durante este curso, descubrí mi pasión por la programación y me interesé por el perfil de programadora Full Stack. Con esto en mente, completé un bootcamp de Front-End Web Development, lo que me ayudó a complementar y ampliar mis conocimientos en ambos entornos.',
-        desc3: 'Además de mis habilidades técnicas, tengo un grado en Estudios de Asia Oriental. Pasé un tiempo estudiando en Japón, donde aprendí a colaborar con personas de diversos orígenes y adaptarme a situaciones nuevas. El arte es otro de mis intereses y continúo desarrollando mis habilidades como artista aficionada en mi tiempo libre.',
+        desc1: 'Soy Anna Escribano, estudiante de desarrollo web.',
+        desc2: 'Empecé a formarme en esta disciplina en 2022, cuando realicé un curso de desarrollo back-end. Durante este curso descubrí mi pasión por la programación y decidí realizar un curso de desarrollo front-end. Actualmente estoy estudiando un Ciclo Superior en Desarrollo Web.',
+        desc3: 'Además de mis habilidades técnicas, tengo un grado en Estudios de Asia Oriental y soy aficionada al dibujo. Pasé un tiempo estudiando en Japón, donde aprendí a colaborar con personas de diversos orígenes y adaptarme a situaciones nuevas.',
         desc4:'Tanto si estoy programando como dibujando, me esfuerzo por llevar la creatividad y la atención al detalle a todo lo que hago.',
         subtitle2: 'Tecnologías y conocimientos'
     }
